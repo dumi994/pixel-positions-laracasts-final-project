28baddceb5ae5ec8aef362f5b5ee4965dcdcd809
Dumi <dumi.9944@gmail.com>
2025-04-02 19:56:29 +0200

    test relazioni ok

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Employer;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
@@ -17,7 +19,15 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id'   => Employer::factory(),
+            'title'         => fake()->jobTitle(),
+            'description'   => fake()->sentence(),
+            'requirements'  => fake()->sentence(), // Aggiungi questa riga
+            'salary'        => fake()->numberBetween(30000, 150000), // Usa numeri per numberBetween
+            'location'      => 'Remote',
+            'schedule'      => 'Full Time',
+            'url'           => fake()->url(),
+            'featured'      => false,
         ];
     }
 }
